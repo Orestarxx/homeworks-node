0326db9a1681b29b05c5d1dcf52987f0ffdfb0cd
Orestarxx <oresto322@gmail.com>
2022-11-10 16:35:06 +0200

    ff

--- app.js
+++ app.js
@@ -23,15 +23,29 @@ const fs = require('node:fs');
 // fs.rmdir('./comp',{recursive:true},(err) =>{
 //     console.log(err);
 // })
-fs.copyFile('./text.txt','./copy.txt', (err) =>{
-    console.log(err);
-})
-
-
-
-
-
-
-
-
+// fs.copyFile('./text.txt','./copy.txt', (err) =>{
+//     console.log(err);
+// })
+//
+// fs.rmdir('./comp',(err) =>{
+//
+// })
 // node app.js
+
+// fs.appendFile('./comp/data.json',JSON.stringify({name:'Arest',age:22}),(err) =>{
+// })
+// fs.appendFile('./student-girls/Nazar.json',JSON.stringify({name:'Nazar',age:22,gender:'Male'}),(err) =>{
+//
+// })
+
+fs.readdir('./student-boys',(err, files) =>{
+    for(const fileName of files){
+        fs.stat(`./student-boys/${fileName}`,(err, stats) =>{
+            if(stats.isFile()){
+                fs.readFile(`./student-boys/${fileName}`,(err, data) =>{
+
+                })
+            }
+        })
+    }
+})
