8c3387d5ca79d7b2185326d0e7afe520c70eaf60
Orestarxx <oresto322@gmail.com>
2022-11-11 07:10:00 +0200

    home work first

--- app.js
+++ app.js
@@ -1,51 +1,35 @@
-const fs = require('node:fs');
-// fs.readFile('./text.txt',(err, data) =>{
-//     console.log(err);
-//     console.log(data.toString());
-// })
-// fs.appendFile('./text.txt','Hello Orest!Nice to meet you\n',(err)=>{
-//
-// })
-// fs.writeFile('./text.txt','My name is Orest',(err) =>{
-//
-// })
-// fs.readFile('./text.txt',(err,data) =>{
-//     fs.appendFile('./copy.txt',data,(err) =>{
-//
-//     })
-// })
-// fs.mkdir('./comp', (err) =>{
-//
-// })
-// fs.appendFile('./comp/data.json',JSON.stringify({name:'Arest',age:22}),(err) =>{
-// })
-// fs.unlink('./comp/data.json',(err) =>{})
-// fs.rmdir('./comp',{recursive:true},(err) =>{
-//     console.log(err);
-// })
-// fs.copyFile('./text.txt','./copy.txt', (err) =>{
-//     console.log(err);
-// })
-//
-// fs.rmdir('./comp',(err) =>{
-//
-// })
-// node app.js
+const fs = require('node:fs').promises;
+const path = require('path')
 
-// fs.appendFile('./comp/data.json',JSON.stringify({name:'Arest',age:22}),(err) =>{
-// })
-// fs.appendFile('./student-girls/Nazar.json',JSON.stringify({name:'Nazar',age:22,gender:'Male'}),(err) =>{
-//
-// })
+// const foo = async () =>{
+//     const folderPath = path.join(__dirname,'student-boys')
+//    const filesGirl = await fs.readdir(folderPath)
+//     for(const file of filesGirl){
+//         const oldPath =path.join(folderPath, file);
+//        const data = await fs.readFile(path.join(oldPath))
+//         const student = JSON.parse(data);
+//         if(student.gender ==='Female'){
+//              await fs.rename(oldPath,path.join(__dirname,'student-girls',file))
+//         }
+//     }
+// }
+const foo = async (readFolder,studentGender,newPath) =>{
+    const folder1Path = path.join(__dirname,readFolder)
 
-fs.readdir('./student-boys',(err, files) =>{
-    for(const fileName of files){
-        fs.stat(`./student-boys/${fileName}`,(err, stats) =>{
-            if(stats.isFile()){
-                fs.readFile(`./student-boys/${fileName}`,(err, data) =>{
+    const files = await fs.readdir(folder1Path)
 
-                })
-            }
-        })
+    for(const file of files){
+
+        const oldPath =path.join(folder1Path, file);
+        const data = await fs.readFile(path.join(oldPath))
+        const student = JSON.parse(data);
+
+        if(student.gender === studentGender){
+            await fs.rename(oldPath,path.join(__dirname,newPath,file))
+
+        }
     }
-})
+}
+
+foo('student-boys','Female','student-girls')
+foo('student-girls','Male','student-boys')
